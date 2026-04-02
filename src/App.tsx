d0be60bf1732e6f862ca1df558b7b09d92637ae6
PawanBangera <pawanbangera142@gmail.com>
2026-04-02 15:17:57 +0530

    feat(project-setup):donw the project setup using react, ts, tailwind, shadcn

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="mx-auto flex min-h-screen max-w-5xl items-center justify-center px-6 py-10">
+        <h1 className="text-2xl font-semibold text-slate-900">PACMAN Dashboard</h1>
+    </div>
+  );
+}
